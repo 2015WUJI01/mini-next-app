b064c08155ddb3535123737139129f3a74d80eff
vonii <wujiankun1998@qq.com>
2025-05-19 03:06:45 +0800

    feat: add icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,28 @@ const inter = Inter({ subsets: ["latin"] })
 export const metadata: Metadata = {
   title: '健康饮食管理',
   description: '记录食谱和规划每日饮食',
+  icons: {
+    icon: [
+      { url: '/favicon_io/favicon.ico' },
+      { url: '/favicon_io/favicon-32x32.png', sizes: '32x32' },
+      { url: '/favicon_io/favicon-16x16.png', sizes: '16x16' },
+    ],
+    apple: [
+      { url: '/favicon_io/apple-touch-icon.png', sizes: '180x180' },
+    ],
+    other: [
+      {
+        rel: 'android-chrome',
+        url: '/favicon_io/android-chrome-192x192.png',
+        sizes: '192x192',
+      },
+      {
+        rel: 'android-chrome',
+        url: '/favicon_io/android-chrome-512x512.png',
+        sizes: '512x512',
+      },
+    ],
+  },
 }
 
 export default function RootLayout({
